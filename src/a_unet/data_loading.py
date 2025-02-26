462451f3634ae695cc064d2041eb782e4b629fd7
julialg2002 <s2107370@ed.ac.uk>
2025-02-26 15:26:13 +0000

    standarised data_loading

--- src/a_unet/data_loading.py
+++ src/a_unet/data_loading.py
@@ -10,10 +10,20 @@ from os import listdir
 from os.path import splitext, isfile, join
 from pathlib import Path
 from torch.utils.data import Dataset
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 from tqdm import tqdm
 import os
 
 def load_image(filename):
+    """This function loads the image file and returns the image.
+    
+    Args:
+        filename (str): The name of the file
+        
+    Returns:
+        PIL.Image: The image
+    """
     ext = splitext(filename)[1]
     if ext == '.npy':
         return Image.fromarray(np.load(filename))
@@ -22,8 +32,20 @@ def load_image(filename):
     else:
         return Image.open(filename)
 
-
 def unique_mask_values(idx, mask_dir, mask_suffix=''):
+    """This function loads the mask file and returns the unique values in the mask file.
+
+    Args:
+        idx (str): The name of the file
+        mask_dir (str): The directory where the mask file is located
+        mask_suffix (str, optional): The suffix of the mask file. Defaults to ''.
+
+    Raises:
+        ValueError: If the mask file has more than 3 dimensions
+
+    Returns:
+        np.array: The unique values in the mask file
+    """
     mask_file = list(mask_dir.glob(idx + mask_suffix + '.*'))[0]
     mask = np.asarray(load_image(mask_file))
     if mask.ndim == 2:
@@ -35,15 +57,95 @@ def unique_mask_values(idx, mask_dir, mask_suffix=''):
     else:
         raise ValueError(f'Loaded masks should have 2 or 3 dimensions, found {mask.ndim}')
 
+class OxfordIIIPetDataset(Dataset):
+    """This class creates a dataset for the Oxford-IIIT Pet dataset. 
+    It also allows for data augmentation through the transform parameter.
+
+    Args:
+        images_dir (str): The directory where the images are located
+        mask_dir (str): The directory where the masks are located
+        mask_suffix (str, optional): The suffix of the mask file. Defaults to ''.
+        transform ([type], optional): The data augmentation transform. Defaults to None.
+        
+    Raises:
+        RuntimeError: If no input file is found in the images directory
+        
+    Returns:
+        dict: The image and mask
+    """
+    def __init__(self, images_dir: str, mask_dir: str, mask_suffix: str = '', transform=None):
+        self.images_dir = Path(images_dir)
+        self.mask_dir = Path(mask_dir)
+        self.mask_suffix = mask_suffix
+        self.transform = transform
+
+        self.ids = [splitext(file)[0] for file in listdir(images_dir) if isfile(join(images_dir, file)) and not file.startswith('.')]
+        if not self.ids:
+            raise RuntimeError(f'No input file found in {images_dir}, make sure you put your images there')
+
+        logging.info(f'Creating dataset with {len(self.ids)} examples')
+        logging.info('Scanning mask files to determine unique values')
+        with Pool() as p:
+            unique = list(tqdm(
+                p.imap(partial(unique_mask_values, mask_dir=self.mask_dir, mask_suffix=self.mask_suffix), self.ids),
+                total=len(self.ids)
+            ))
+
+        self.mask_values = list(sorted(np.unique(np.concatenate(unique), axis=0).tolist()))
+        logging.info(f'Unique mask values: {self.mask_values}')
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __getitem__(self, idx):
+        name = self.ids[idx]
+        mask_file = list(self.mask_dir.glob(name + self.mask_suffix + '.*'))
+        img_file = list(self.images_dir.glob(name + '.*'))
+
+        assert len(img_file) == 1, f'Either no image or multiple images found for the ID {name}: {img_file}'
+        assert len(mask_file) == 1, f'Either no mask or multiple masks found for the ID {name}: {mask_file}'
+        
+        mask = load_image(mask_file[0])
+        img = load_image(img_file[0])
+
+        assert img.size == mask.size, \
+            f'Image and mask {name} should be the same size, but are {img.size} and {mask.size}'
+
+        if not self.transform:
+            # Defaultly resize the image to 256x256 and normalize it
+            self.transform  = A.Compose([
+                A.Resize(256, 256),
+                A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+                ToTensorV2()
+            ])
+            
+        
+        img = np.asarray(img)
+        mask = np.asarray(mask)
+        
+        # Remove the alpha channel if it exists
+        if img.ndim == 3 and img.shape[2] == 4:
+            img = img[:,:,:3]
+            
+        # Apply the transform
+        augmented = self.transform(image=img, mask=mask)
+
+        return {
+            'image': augmented['image'],
+            'mask': augmented['mask']
+        }
+        
+
 
 class BasicDataset(Dataset):
-    def __init__(self, images_dir: str, mask_dir: str, scale: float = 1.0, mask_suffix: str = '', transform=None):
+    """This class creates a basic dataset. It allows image scaling but does not have data augmentation."""
+    
+    def __init__(self, images_dir: str, mask_dir: str, scale: float = 1.0, mask_suffix: str = ''):
         self.images_dir = Path(images_dir)
         self.mask_dir = Path(mask_dir)
         assert 0 < scale <= 1, 'Scale must be between 0 and 1'
         self.scale = scale
         self.mask_suffix = mask_suffix
-        self.transform = transform
 
         self.ids = [splitext(file)[0] for file in listdir(images_dir) if isfile(join(images_dir, file)) and not file.startswith('.')]
         if not self.ids:
@@ -82,9 +184,7 @@ class BasicDataset(Dataset):
             return mask
 
         else:
-            if img.ndim == 3 and img.shape[0] == 4:
-                img = img[:3,:,:]
-            elif img.ndim == 2:
+            if img.ndim == 2:
                 img = img[np.newaxis, ...]
             else:
                 img = img.transpose((2, 0, 1))
@@ -107,72 +207,10 @@ class BasicDataset(Dataset):
         assert img.size == mask.size, \
             f'Image and mask {name} should be the same size, but are {img.size} and {mask.size}'
 
-        if self.transform:
-            img = np.asarray(img)
-            mask = np.asarray(mask)
-            if img.ndim == 3 and img.shape[2] == 4:
-                img = img[:,:,:3]
-            augmented = self.transform(image=img, mask=mask)
-            img, mask = augmented['image'], augmented['mask']
-            # check that all images have dimensions (128, 128, 3)
-            # and all masks have dimensions (128, 128)
-            assert img.shape == torch.Size([3, 256, 256]), f'Image {name} has shape {img.shape}'
-            assert mask.shape == torch.Size([256, 256]), f'Mask has shape {mask.shape}'
-
-        else:
-            img = self.preprocess(self.mask_values, img, self.scale, is_mask=False)
-            mask = self.preprocess(self.mask_values, mask, self.scale, is_mask=True)
+        img = self.preprocess(self.mask_values, img, self.scale, is_mask=False)
+        mask = self.preprocess(self.mask_values, mask, self.scale, is_mask=True)
 
         return {
-            'image': img,
-            'mask': mask
+            'image': torch.as_tensor(img.copy()).float().contiguous(),
+            'mask': torch.as_tensor(mask.copy()).long().contiguous()
         }
-
-
-
-class SegmentationDataset(Dataset):
-    def __init__(self, image_dir, mask_dir, transform=None):
-        self.image_dir = image_dir
-        self.mask_dir = mask_dir
-        self.transform = transform
-        self.image_filenames = os.listdir(image_dir)
-    
-    def __len__(self):
-        return len(self.image_filenames)
-    
-    def get_label_filename(self, idx):
-        return self.image_filenames[idx].replace(".jpg", ".png")
-    
-    def __getitem__(self, idx):
-        image_path = os.path.join(self.image_dir, self.image_filenames[idx])
-        mask_path = os.path.join(self.mask_dir, self.get_label_filename(idx))
-
-        image = imread(image_path)
-        mask = imread(mask_path)
-
-        # Convert RGBA to RGB if necessary
-        if image.shape[2] == 4:
-            image = image[:, :, :3]
-        
-        if self.transform:
-            augmented = self.transform(image=image, mask=mask)
-            image, mask = augmented["image"], augmented["mask"]
-
-        # Convert masks from (H, W, 3) to (H, W) based on the color of the pixel
-        COLOR_MAP = {
-            (0, 0, 0): 0,  # Background
-            (128, 0, 0): 1,  # Red (Cat)
-            (0, 128, 0): 2  # Green (Dog)
-        }
-
-        label_mask = np.full(mask.shape[:2], 3, dtype=np.uint8)
-
-        for rgb, label in COLOR_MAP.items():
-            mask_pixels = (mask[:, :, 0] == rgb[0]) & (mask[:, :, 1] == rgb[1]) & (mask[:, :, 2] == rgb[2])
-            label_mask[mask_pixels] = label
-
-        return {
-            'image': image,
-            'mask': label_mask
-        }
-
