7696bcb305d27fb1c7039efd7a9c5a61706f6f05
julialg2002 <s2107370@ed.ac.uk>
2025-04-09 06:12:17 +0100

    fixed typo

--- src/models/clip_model.py
+++ src/models/clip_model.py
@@ -204,13 +204,13 @@ class Up(nn.Module):
         self.use_skip = use_skip
 
         # if bilinear, use the normal convolutions to reduce the number of channels
-         if bilinear:
-             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-             self.conv = DoubleConv(in_channels, out_channels, in_channels // 2, dropout_rate=dropout_rate)
-         else:
-             self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-             new_in_channels = in_channels if use_skip else in_channels // 2
-             self.conv = DoubleConv(new_in_channels, out_channels, dropout_rate=dropout_rate)
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2, dropout_rate=dropout_rate)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+            new_in_channels = in_channels if use_skip else in_channels // 2
+            self.conv = DoubleConv(new_in_channels, out_channels, dropout_rate=dropout_rate)
 
     def forward(self, x1, x2):
         x1 = self.up(x1) # now it has 512
